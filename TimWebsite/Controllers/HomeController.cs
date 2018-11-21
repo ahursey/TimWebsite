db5cb8f3363b5439787a5dc7fb22a53bfdd8924b
Aaron Hursey <hursey.aaron@gmail.com>
2018-11-21 12:47:05 -0800

    Initial commit

---
+++ TimWebsite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CV()
+        {
+            ViewBag.Message = "Curriculum Vitae";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Me";
+
+            return View();
+        }
+
+        public ActionResult Research()
+        {
+            ViewBag.Message = "My Research";
+
+            return View();
+        }
+    }
+}
